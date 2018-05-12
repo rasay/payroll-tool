b305b0980987110fd6e1fdfa51f8edd78008b1e5
Ross Asay <rkasay@gmail.com>
2018-05-12 15:30:48 -0600

    Added UI.

--- src/main/java/com/asayclips/App.java
+++ src/main/java/com/asayclips/App.java
@@ -5,6 +5,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+import javafx.scene.control.TextArea;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
@@ -16,20 +17,26 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 public class App 
 {
     private static String _userHome = System.getProperty("user.home");
+    private TextArea _messageArea;
+
+    public App(TextArea messageArea)
+    {
+        _messageArea = messageArea;
+    }
 
     public static void main( String[] args )
     {
-        if (args.length != 3)
+        if (args.length != 2)
         {
-            System.err.println("Usage: {begin MM/DD/YYYY} {store# e.g. UT104} {output}");
+            System.err.println("Usage: {begin MM/DD/YYYY} {store# e.g. UT104}");
             System.exit(1);
         }
         String firstDay = args[0];
         String storeNumber = args[1];
-        String payrollFile = args[2];
 
-        App app = new App();
-        app.generatePayroll(firstDay, storeNumber, payrollFile);
+        App app = new App(null);
+
+        app.generatePayroll(firstDay, storeNumber);
     }
 
     class Stylist
@@ -58,28 +65,46 @@ public class App
         }
     }
 
-    public void generatePayroll(String firstDay, String storeNumber, String payrollFile)
+    public void generatePayroll(String endDate, String storeNumber)
     {
         try
         {
-            String lastDay = findOffsetDate(firstDay, 13);
-            File firstWeekStylistAnalysis = findFirstWeekStylistAnalysis(firstDay, storeNumber);
-            File fullPeriodStylistAnalysis = findStylistAnalysis(firstDay, lastDay, storeNumber);
-            File tipsFile = findTips(firstDay, lastDay, storeNumber);
+            displayMessage("### Processing: " + storeNumber + ", " + endDate);
+            // ensure correct format for storeNumber and endDate
+            endDate = findOffsetDate(endDate, 0);
+            storeNumber = storeNumber.trim().toUpperCase();
 
-            Map<String, Stylist> stylists = readData(firstWeekStylistAnalysis, fullPeriodStylistAnalysis, tipsFile);
+            String beginDate = findOffsetDate(endDate, -13);
+            String outputFilename = String.format("%s/Documents/Payroll/%s_%s.xls",
+                    _userHome, storeNumber, endDate.replaceAll("/", "-"));
 
-//            for (Stylist stylist : stylists.values())
-//                System.out.println(stylist);
+            File firstWeekStylistAnalysis = findFirstWeekStylistAnalysis(beginDate, storeNumber);
+            File fullPeriodStylistAnalysis = findStylistAnalysis(beginDate, endDate, storeNumber);
+            File tipsFile = findTips(beginDate, endDate, storeNumber);
 
-            String templateFile = String.format("%s/Documents/Payroll Templates/%s.xls", _userHome, storeNumber);
-            buildPayrollFile(templateFile, payrollFile, stylists, lastDay);
+            if (firstWeekStylistAnalysis == null || fullPeriodStylistAnalysis == null || tipsFile == null)
+                displayMessage("### Failure!!!");
+            else
+            {
+                Map<String, Stylist> stylists = readData(firstWeekStylistAnalysis, fullPeriodStylistAnalysis, tipsFile);
+                String templateFile = String.format("%s/Documents/Payroll Templates/%s.xls", _userHome, storeNumber);
+                buildPayrollFile(templateFile, outputFilename, stylists, endDate);
+                displayMessage("### Success!!!");
+            }
         }
         catch (Exception e)
         {
-            System.out.println("ERROR : " + e.toString());
+            displayMessage("ERROR : " + e.toString());
             e.printStackTrace();
         }
+    }
+
+    private void displayMessage(String message)
+    {
+        if (_messageArea == null)
+            System.out.println(message);
+        else
+            _messageArea.appendText(message + "\n");
     }
 
     private Map<String, Stylist> readData(File firstWeekStylistAnalysis, File fullPeriodStylistAnalysis, File tipsFile) throws Exception
@@ -91,12 +116,12 @@ public class App
         return stylists;
     }
 
-    private File findFirstWeekStylistAnalysis(String firstDay, String storeNumber) throws Exception
+    private File findFirstWeekStylistAnalysis(String beginDate, String storeNumber) throws Exception
     {
-        return findStylistAnalysis(firstDay, findOffsetDate(firstDay, 6), storeNumber);
+        return findStylistAnalysis(beginDate, findOffsetDate(beginDate, 6), storeNumber);
     }
 
-    private File findStylistAnalysis(String firstDay, String lastDay, String storeNumber) throws Exception
+    private File findStylistAnalysis(String beginDate, String endDate, String storeNumber) throws Exception
     {
         File file = null;
         File dir = new File(_userHome + "/Downloads/");
@@ -104,21 +129,22 @@ public class App
         {
             if (f.getName().startsWith("Stylist_Analysis")
                     && f.getName().endsWith(".xls")
-                    && isTheRightStylistAnalysisFile(f, firstDay, lastDay, storeNumber))
+                    && isTheRightStylistAnalysisFile(f, beginDate, endDate, storeNumber))
             {
-                System.out.printf("Found stylist analysis report for store: %s, (%s - %s) %s\n",
-                        storeNumber, firstDay, lastDay, f.getAbsolutePath());
+                displayMessage(String.format("Found stylist analysis report for store: %s, (%s - %s) %s",
+                        storeNumber, beginDate, endDate, f.getAbsolutePath()));
                 file = f;
+                break;
             }
         }
         if (file == null)
-            System.out.printf("Download stylist analysis report for store: %s, (%s - %s)\n",
-                    storeNumber, firstDay, lastDay);
+            displayMessage(String.format("Download stylist analysis report for store: %s, (%s - %s)",
+                    storeNumber, beginDate, endDate));
 
         return file;
     }
 
-    private boolean isTheRightStylistAnalysisFile(File file, String firstDay, String lastDay, String storeNumber) throws Exception
+    private boolean isTheRightStylistAnalysisFile(File file, String beginDate, String endDate, String storeNumber) throws Exception
     {
         FileInputStream inputStream = new FileInputStream(file);
         try
@@ -127,7 +153,7 @@ public class App
             HSSFSheet sheet = workbook.getSheet("Worksheet");
 
             HSSFRow row = sheet.getRow(1);
-            if (!String.format("%s - %s", firstDay, lastDay).equals(row.getCell(1).getStringCellValue()))
+            if (!String.format("%s - %s", beginDate, endDate).equals(row.getCell(1).getStringCellValue()))
                 return false;
 
             return storeNumber.equals(row.getCell(2).getStringCellValue());
@@ -139,13 +165,13 @@ public class App
     }
 
     private static SimpleDateFormat _dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-    private static String findOffsetDate(String firstDay, int days) throws ParseException
+    private static String findOffsetDate(String baseDate, int days) throws ParseException
     {
-        Date d = new Date(_dateFormat.parse(firstDay).getTime() + days*24*60*60*1000);
+        Date d = new Date(_dateFormat.parse(baseDate).getTime() + days*24*60*60*1000);
         return _dateFormat.format(d);
     }
 
-    private File findTips(String firstDay, String lastDay, String storeNumber) throws Exception
+    private File findTips(String beginDate, String endDate, String storeNumber) throws Exception
     {
         File file = null;
         File dir = new File(_userHome + "/Downloads/");
@@ -153,21 +179,21 @@ public class App
         {
             if (f.getName().startsWith("Tips_By_Employee_Report")
                     && f.getName().endsWith(".xls")
-                    && isTheRightTipsFile(f, firstDay, lastDay, storeNumber))
+                    && isTheRightTipsFile(f, beginDate, endDate, storeNumber))
             {
                 file = f;
-                System.out.printf("Found tips by employee report for store: %s (%s - %s) %s\n",
-                        storeNumber, firstDay, lastDay, f.getAbsolutePath());
+                displayMessage(String.format("Found tips by employee report for store: %s (%s - %s) %s",
+                        storeNumber, beginDate, endDate, f.getAbsolutePath()));
             }
         }
         if (file == null)
-            System.out.printf("Download tips by employee report for store: %s (%s - %s)\n",
-                    storeNumber, firstDay, lastDay);
+            displayMessage(String.format("Download tips by employee report for store: %s (%s - %s)",
+                    storeNumber, beginDate, endDate));
 
         return file;
     }
 
-    private boolean isTheRightTipsFile(File file, String firstDay, String lastDay, String storeNumber) throws Exception
+    private boolean isTheRightTipsFile(File file, String beginDate, String endDate, String storeNumber) throws Exception
     {
         FileInputStream inputStream = new FileInputStream(file);
         try
@@ -176,7 +202,7 @@ public class App
             HSSFSheet sheet = workbook.getSheet("Worksheet");
 
             HSSFRow row = sheet.getRow(1);
-            if (!String.format("%s - %s", firstDay, lastDay).equals(row.getCell(1).getStringCellValue()))
+            if (!String.format("%s - %s", beginDate, endDate).equals(row.getCell(1).getStringCellValue()))
                 return false;
 
             row = sheet.getRow(0);
@@ -188,12 +214,12 @@ public class App
         }
     }
 
-    private void buildPayrollFile(String templateFilename, String payrollFilename, Map<String,Stylist> stylists, String lastDay) throws Exception
+    private void buildPayrollFile(String templateFilename, String payrollFilename, Map<String,Stylist> stylists, String endDate) throws Exception
     {
         FileInputStream inputStream = new FileInputStream(new File(templateFilename));
         HSSFWorkbook workbook = new HSSFWorkbook(inputStream);
 
-        setPayrollDate(workbook.getSheet("Information"), lastDay);
+        setPayrollDate(workbook.getSheet("Information"), endDate);
 
         HSSFSheet sheet = workbook.getSheet("Template");
         processManager(sheet, stylists);
@@ -223,7 +249,7 @@ public class App
         Stylist manager = stylists.get(name.toLowerCase());
         if (manager == null)
         {
-            System.out.printf("No match for manager (%s) found in stylist analysis reports.\n", name);
+            displayMessage(String.format("No match for manager (%s) found in stylist analysis reports.", name));
             return;
         }
 
@@ -269,7 +295,7 @@ public class App
                 stylist.foundMatch = true;
             }
             else if (name != null && !name.equals("") && !name.contains("Stylist"))
-                System.out.printf("No match for stylist (%s) found in stylist analysis report.\n", name);
+                displayMessage(String.format("No match for stylist (%s) found in stylist analysis report.", name));
         }
     }
 
@@ -278,7 +304,7 @@ public class App
         for (Stylist employee : employees.values())
         {
             if (!employee.foundMatch && !"Business House".equals(employee.name) && !"Total Salon".equals(employee.name))
-                System.out.printf("No match for employee (%s) found in payroll template.\n", employee.name);
+                displayMessage(String.format("No match for employee (%s) found in payroll template.", employee.name));
         }
     }
 
@@ -299,7 +325,7 @@ public class App
                 receptionist.foundMatch = true;
             }
             else if (name != null && !name.equals("") && !name.contains("Coordinator"))
-                System.out.printf("No match for coordinator (%s) found in stylist analysis report.\n", name);
+                displayMessage(String.format("No match for coordinator (%s) found in stylist analysis report.", name));
         }
     }
 
--- src/main/java/com/asayclips/Form.java
+++ src/main/java/com/asayclips/Form.java
@@ -1,4 +1,72 @@
 package com.asayclips;
 
-public class Form {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+
+import javafx.scene.layout.Background;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Form extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Sport Clips Payroll");
+
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+
+        // first row
+        final Label datePickerLabel = new Label("Payroll End Date");
+        gridPane.add(datePickerLabel, 0, 0);
+
+        final DatePicker datePicker = new DatePicker();
+        gridPane.add(datePicker, 0, 1);
+
+        // second row
+        final Label storePickerLabel = new Label("Store #");
+        gridPane.add(storePickerLabel, 0, 3);
+
+        final TextField storeText = new TextField();
+        gridPane.add(storeText, 0, 4);
+
+        // message area
+        final TextArea messageArea = new TextArea();
+        messageArea.backgroundProperty().setValue(Background.EMPTY);
+        gridPane.add(messageArea, 0, 6);
+
+        //bottom
+        Button goButton = new Button();
+        goButton.setText("Process");
+        goButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent event) {
+                App app = new App(messageArea);
+                app.generatePayroll(datePicker.getEditor().getText(), storeText.getText());
+            }
+        });
+        gridPane.add(goButton, 0, 15);
+
+
+        Button exitButton = new Button();
+        exitButton.setText("Exit");
+        exitButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent event) {
+                System.exit(0);
+            }
+        });
+        gridPane.add(exitButton, 0, 16);
+
+        primaryStage.setScene(new Scene(gridPane, 600, 500));
+        primaryStage.show();
+    }
 }
